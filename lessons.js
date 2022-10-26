8e2a06f805748e87bc364273ea67fda4c3da77b5
NathanielLaylo <laylonathan1@yahoo.com>
2022-10-26 20:01:36 +0100

    Update lessons.js
    added dummy data for the lesson

---
+++ lessons.js
@@ -0,0 +1,82 @@
+var lessons = [
+    {
+        id: 1001,
+        subject: "Math",
+        location: "London",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1002,
+        subject: "English",
+        location: "Borehamwood",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1003,
+        subject: "Science",
+        location: "Watford",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1004,
+        subject: "Math",
+        location: "London",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1005,
+        subject: "English",
+        location: "Borehamwood",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1006,
+        subject: "Science",
+        location: "Watford",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1007,
+        subject: "Math",
+        location: "London",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1008,
+        subject: "English",
+        location: "Borehamwood",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1009,
+        subject: "Science",
+        location: "Watford",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    },
+    {
+        id: 1010,
+        subject: "Science",
+        location: "Watford",
+        price: 2000,
+        space: 5,
+        image: "./Images/math.png"
+    }
+];
